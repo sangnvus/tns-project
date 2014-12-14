ce5ce7d4da1f2232af68f16e4698e9284d587f56
hieudtse02895@fpt.edu.vn <hieudtse02895@fpt.edu.vn@88ba3ff6-d7b0-eaa4-419e-cdb00ae9c095>
2014-12-14 12:16:56 +0000

    update

--- WIP/Users/hieugie/TaxiNet/src/hieugie/capstone/taxinet/FavoriteTaxiFragment.java
+++ WIP/Users/hieugie/TaxiNet/src/hieugie/capstone/taxinet/FavoriteTaxiFragment.java
@@ -6,10 +6,13 @@ import hieugie.capstone.taxinet.model.FavoriteDriverItem;
 import java.util.ArrayList;
 
 import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 public class FavoriteTaxiFragment extends Fragment {
@@ -18,22 +21,39 @@ public class FavoriteTaxiFragment extends Fragment {
 	}
 
 	private ListView mFavouriteDriverList;
-	private ArrayList<FavoriteDriverItem> driverItems = new ArrayList<FavoriteDriverItem>();
+	private ArrayList<FavoriteDriverItem> favoriteDriverItems = new ArrayList<FavoriteDriverItem>();
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
 
-		View rootView = inflater.inflate(R.layout.fragment_favorite_taxi,
+		final View rootView = inflater.inflate(R.layout.fragment_favorite_taxi,
 				container, false);
 		mFavouriteDriverList = (ListView) rootView
 				.findViewById(R.id.favorite_taxi_list);
-		driverItems.add(new FavoriteDriverItem(1, "Đào Trung hiếu", true));
-		driverItems.add(new FavoriteDriverItem(2, "Đinh Quang Dương", false));
+		favoriteDriverItems.add(new FavoriteDriverItem(1, "Đào Trung hiếu",
+				"01683449693", true));
+		favoriteDriverItems.add(new FavoriteDriverItem(2, "Đinh Quang Dương",
+				"043113", false));
 		final DriverAdapter driverAdapter = new DriverAdapter(
 				rootView.getContext(), R.layout.favorite_driver_list_item,
-				driverItems);
+				favoriteDriverItems);
 		mFavouriteDriverList.setAdapter(driverAdapter);
+
+		mFavouriteDriverList.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View arg1,
+					int position, long duration) {
+				Intent it = new Intent(rootView.getContext(),
+						FavoriteDriverDetails.class);
+				it.putExtra("favorite driver item",
+						favoriteDriverItems.get(position));
+				startActivity(it);
+			}
+
+		});
+
 		return rootView;
 	}
 }
